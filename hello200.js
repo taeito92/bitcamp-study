5580763c4d9e7e72e4b1b4d1b2f96af96c16bacc
hannaleve <wbtmgkssk@naver.com>
2021-06-09 16:46:16 +0900

    lorr

--- hello200.js
+++ hello200.js
@@ -2,14 +2,7 @@ console.log("테스트2");
 console.log("테스트3asdasd");
 console.log("테스트4");
 console.log("테스트5qweda");
-console.log("테스트6");
-console.log("테스트8");
-console.log("테스트9");
-console.log("테스트10");
-console.log("테스트10qq");
-console.log("테스트11qq");
+console.log("==> 수정!!");
 
-console.log("테스트102q");
 
-console.log("테스트10qq");
 
