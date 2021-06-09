b2bf8a892ee213e27d25d06c8fb97858e10f4398
taeito92 <bethest4r@gmail.com>
2021-06-09 16:02:08 +0900

    100

---
+++ hello100.js
@@ -0,0 +1,8 @@
+console.log("Hello, world2!"); 
+console.log("B 개발자");
+console.log(" A 개발자!");
+console.log(" b 개발자!");
+console.log(" c 개발자!");
+console.log(" d 개발자!");
+console.log(" f 개발자!");
+console.log(" g 개발자!");
