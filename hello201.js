b665e00d6e874c9437153892c6a447460a74ede7
hannaleve <wbtmgkssk@naver.com>
2021-06-09 16:21:33 +0900

    message

--- hello201.js
+++ hello201.js
@@ -1,8 +1,7 @@
-<<<<<<< HEAD
+
 var name = "홍길동";
 var age = 10;
 console.log("나의 이름은 : " + name + "입니다, 나이는 : " + age + "입니다."); 
-=======
 console.log("테스트1");
 console.log("테스트2");
 console.log("테스트3");
@@ -13,4 +12,4 @@ console.log("테스트7");
 console.log("테스트8");
 console.log("테스트9");
 console.log("테스트10");
->>>>>>> e356a5ee25ab4a75bd2bd62c2fd7d2f21880130f
+
