7834a4fe5f5990f6424009e773a0039915fdd8dc
taeito92 <bethest4r@gmail.com>
2021-06-09 15:09:00 +0900

    .

--- hello2.js
+++ hello2.js
@@ -1,4 +1,5 @@
- console.log("Hello, world!");  
+console.log("나는야 a 개발자");  
+console.log("Hello, world!");  
 console.log("나는야 b 개발자");
- var studentName = "홍길동";
- console.log("이름: "+ studentName);
+var studentName = "홍길동";
+console.log("이름: "+ studentName);
