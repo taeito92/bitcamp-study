6fa33247336023afc45aca9c63b9e98e3a65f8a1
taeito92 <bethest4r@gmail.com>
2021-06-10 10:12:50 +0900

    .

--- hello2.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("Hello, World2!");
-    return 0;
-}
--- hello3.js
+++
@@ -1,6 +0,0 @@
-consloe.log("aaaaaaaaaa");
-console.log("Hello, world!");  
-console.log("aaaaaaaaaa");
-console.log("bbbbbbbbbbbbb");
-console.log("Hello, world!"); 
-console.log("bbbbbbbb");
